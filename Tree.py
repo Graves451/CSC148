8137f87a116687ef3291db3b14f49d1f2aee50f0
Frunk Peng <46289416+Graves451@users.noreply.github.com>
2022-11-01 22:40:39 -0400

    implemented deletion tool for tree

--- Tree.py
+++ Tree.py
@@ -3,6 +3,33 @@ class Tree:
         self._root = root
         self._subtrees = subtrees
 
+    def delete_item(self, target) -> bool:
+        if self.is_empty():
+            return False
+        elif self._subtrees == []:
+            if self._root == target:
+                self._root = None
+                return True
+            return False
+        else:
+            if self._root == target:
+                self._delete_root()
+                return True
+            ans = False
+            for nodes in self._subtrees:
+                bool = nodes.delete_item(target)
+                if bool and nodes._subtrees == []:
+                    self._subtrees.remove(nodes)
+                    return True
+                elif bool:
+                    return True
+            return ans
+
+    def _delete_root(self):
+        chose_tree = self._subtrees.pop()
+        self._root = chose_tree._root
+        self._subtrees.extend(chose_tree._subtrees)
+
     def is_empty(self):
         return self._root is None
 
@@ -39,5 +66,9 @@ len(t2)
 >>> 3
 """
 
-t2 = Tree(3, [Tree(4, []), Tree(1, [])])
-print(str(t2))
+t = Tree(10, [Tree(1, []), Tree(2, []), Tree(3, [])])  # A tree with leaves 1, 2, and 3
+print(t.delete_item(1))
+print(t.delete_item(2))
+print(t.delete_item(3))
+print(t._subtrees)
+print(t._subtrees[0].is_empty() and t._subtrees[1].is_empty() and t._subtrees[2].is_empty())
